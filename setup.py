880834416579516e1b1b7bbc1641b82a7e7f5f2e
spyrostz <spyros@gridsingularity.com>
2020-10-07 13:17:57 +0200

    Added default version as 0.9.20 in case the version is not defined at all.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ elif environ.get('GITHUB_REF'):
 
     version = environ['GITHUB_REF'].replace('refs/tags/v', '')
 else:
-    raise ValueError('Missing commit tag, can\'t set version')
+    version = "0.9.20"
 
 here = path.abspath(path.dirname(__file__))
 
